3624cff1749839a822fce64bdd13fc8d0d7fa18a
Hux-wq <liscabomarkryan.09172002@gmail.com>
2025-03-30 00:18:20 +0800

    update report dl

--- app/Http/Controllers/DataExportController.php
+++ app/Http/Controllers/DataExportController.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use PhpOffice\PhpSpreadsheet\Spreadsheet;
-use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 use Illuminate\Support\Facades\Response;
+use OpenSpout\Writer\XLSX\Writer;
+use OpenSpout\Common\Entity\Row;
 use Carbon\Carbon;
 use App\Models\Student;
 
@@ -13,52 +13,63 @@ class DataExportController extends Controller
 {
     public function exportStudents()
     {
-        // Fetch users
-        $users = Student::all();
-
-        // Create new Spreadsheet
-        $spreadsheet = new Spreadsheet();
-        $sheet = $spreadsheet->getActiveSheet();
-
-        // Set headers
-        $sheet->setCellValue('A1', 'ID');
-        $sheet->setCellValue('B1', 'Name');
-        $sheet->setCellValue('C1', 'Email');
-        $sheet->setCellValue('D1', 'Gender');
-        $sheet->setCellValue('E1', 'Birth Date');
-        $sheet->setCellValue('F1', 'Street Address');
-
-        // Style header row
-        $sheet->getStyle('A1:D1')->getFont()->setBold(true);
-
-        // Add data rows
-        $row = 2;
-        foreach ($users as $user) {
-            $fullname = $user->last_name .' '. $user->suffix_name .', ' . $user->first_name .' '. $user->middle_name;
-            $sheet->setCellValue('A' . $row, $user->student_number);
-            $sheet->setCellValue('B' . $row, $fullname);
-            $sheet->setCellValue('C' . $row, $user->email_address);
-            $sheet->setCellValue('D' . $row, $user->gender);
-            $sheet->setCellValue('E' . $row, $user->birthdate ? \Carbon\Carbon::parse($user->birthdate)->format('M d, Y') : '');
-            $sheet->setCellValue('E' . $row, $user->current_address);
-            $row++;
+        $directory = storage_path('app/exports');
+        if (!file_exists($directory)) {
+            mkdir($directory, 0755, true);
         }
+        $timestamp = Carbon::now()->format('Ymd_His');
+        $filePath = storage_path("app/exports/students_{$timestamp}.xlsx");
+        $writer = new Writer();
 
-        // Auto-size columns
-        foreach (range('A', 'D') as $columnID) {
-            $sheet->getColumnDimension($columnID)->setAutoSize(true);
+        $writer->openToFile($filePath);
+
+        $writer->addRow(Row::fromValues(['id','name','email','age','birthdate','address']));
+
+        Student::cursor()->each(function ($user) use ($writer){
+            $writer->addRow(Row::fromValues([
+                $user->student_number,
+                $user->last_name .' '. $user->suffix_name.', '. $user->first_name.' '. $user->middle_name,
+                $user->email_address,
+                $user->age,
+                $user->birthdate,
+                $user->current_address,
+            ]));
+        });
+
+        $writer->close();
+
+        return response()->download($filePath);
+
+    }
+
+    public function exportFilteredStudents($year)
+    {
+        $directory = storage_path('app/exports');
+        if (!file_exists($directory)) {
+            mkdir($directory, 0755, true);
         }
+        $timestamp = Carbon::now()->format('Ymd_His');
+        $filePath = storage_path("app/exports/students_{$timestamp}.xlsx");
+        $writer = new Writer();
 
-        // Generate file
-        $filename = 'students_export_' . now()->format('YmdHis') . '.xlsx';
-        $filepath = storage_path('app/' . $filename);
+        $writer->openToFile($filePath);
 
-        // Save file
-        $writer = new Xlsx($spreadsheet);
-        $writer->save($filepath);
+        $writer->addRow(Row::fromValues(['id','name','email','age','birthdate','address']));
 
-        // Download response
-        return Response::download($filepath, $filename)
-            ->deleteFileAfterSend(true);
+        Student::where('academic_year', $year)->cursor()->each(function ($user) use ($writer){
+            $writer->addRow(Row::fromValues([
+                $user->student_number,
+                $user->last_name .' '. $user->suffix_name.', '. $user->first_name.' '. $user->middle_name,
+                $user->email_address,
+                $user->age,
+                $user->birthdate,
+                $user->current_address,
+            ]));
+        });
+
+        $writer->close();
+
+        return response()->download($filePath);
+
     }
 }
