381761c1eccb2b0de1541273db139fa4bbb8a788
Hux-wq <liscabomarkryan.09172002@gmail.com>
2025-03-30 01:17:37 +0800

    update dl 3

--- app/Http/Controllers/DataExportController.php
+++ app/Http/Controllers/DataExportController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
-// use OpenSpout\Writer\XLSX\Writer;
-// use OpenSpout\Common\Entity\Row;
+use OpenSpout\Writer\XLSX\Writer;
+use OpenSpout\Common\Entity\Row;
 use Rap2hpoutre\FastExcel\FastExcel;
 use Carbon\Carbon;
 use App\Models\Student;
@@ -14,70 +14,63 @@ class DataExportController extends Controller
 {
     public function exportStudents()
     {
-         // Retrieve the data you want to export
-         $users = Student::all();
+        $directory = storage_path('app/exports');
+        if (!file_exists($directory)) {
+            mkdir($directory, 0755, true);
+        }
+        $timestamp = Carbon::now()->format('Ymd_His');
+        $filePath = storage_path("app/exports/students_{$timestamp}.xlsx");
+        $writer = new Writer();
 
-         // Export to an Excel file
-         return (new FastExcel($users))->download('users.xlsx');
+        $writer->openToFile($filePath);
+
+        $writer->addRow(Row::fromValues(['id','name','email','age','birthdate','address']));
+
+        Student::cursor()->each(function ($user) use ($writer){
+            $writer->addRow(Row::fromValues([
+                $user->student_number,
+                $user->last_name .' '. $user->suffix_name.', '. $user->first_name.' '. $user->middle_name,
+                $user->email_address,
+                $user->age,
+                $user->birthdate,
+                $user->current_address,
+            ]));
+        });
+
+        $writer->close();
+
+        return response()->download($filePath);
+
     }
-    // {
-    //     $directory = storage_path('app/exports');
-    //     if (!file_exists($directory)) {
-    //         mkdir($directory, 0755, true);
-    //     }
-    //     $timestamp = Carbon::now()->format('Ymd_His');
-    //     $filePath = storage_path("app/exports/students_{$timestamp}.xlsx");
-    //     $writer = new Writer();
 
-    //     $writer->openToFile($filePath);
+    public function exportFilteredStudents($year)
+    {
+        $directory = storage_path('app/exports');
+        if (!file_exists($directory)) {
+            mkdir($directory, 0755, true);
+        }
+        $timestamp = Carbon::now()->format('Ymd_His');
+        $filePath = storage_path("app/exports/students_{$timestamp}.xlsx");
+        $writer = new Writer();
 
-    //     $writer->addRow(Row::fromValues(['id','name','email','age','birthdate','address']));
+        $writer->openToFile($filePath);
 
-    //     Student::cursor()->each(function ($user) use ($writer){
-    //         $writer->addRow(Row::fromValues([
-    //             $user->student_number,
-    //             $user->last_name .' '. $user->suffix_name.', '. $user->first_name.' '. $user->middle_name,
-    //             $user->email_address,
-    //             $user->age,
-    //             $user->birthdate,
-    //             $user->current_address,
-    //         ]));
-    //     });
+        $writer->addRow(Row::fromValues(['id','name','email','age','birthdate','address']));
 
-    //     $writer->close();
+        Student::where('academic_year', $year)->cursor()->each(function ($user) use ($writer){
+            $writer->addRow(Row::fromValues([
+                $user->student_number,
+                $user->last_name .' '. $user->suffix_name.', '. $user->first_name.' '. $user->middle_name,
+                $user->email_address,
+                $user->age,
+                $user->birthdate,
+                $user->current_address,
+            ]));
+        });
 
-    //     return response()->download($filePath);
+        $writer->close();
 
-    // }
+        return response()->download($filePath);
 
-    // public function exportFilteredStudents($year)
-    // {
-    //     $directory = storage_path('app/exports');
-    //     if (!file_exists($directory)) {
-    //         mkdir($directory, 0755, true);
-    //     }
-    //     $timestamp = Carbon::now()->format('Ymd_His');
-    //     $filePath = storage_path("app/exports/students_{$timestamp}.xlsx");
-    //     $writer = new Writer();
-
-    //     $writer->openToFile($filePath);
-
-    //     $writer->addRow(Row::fromValues(['id','name','email','age','birthdate','address']));
-
-    //     Student::where('academic_year', $year)->cursor()->each(function ($user) use ($writer){
-    //         $writer->addRow(Row::fromValues([
-    //             $user->student_number,
-    //             $user->last_name .' '. $user->suffix_name.', '. $user->first_name.' '. $user->middle_name,
-    //             $user->email_address,
-    //             $user->age,
-    //             $user->birthdate,
-    //             $user->current_address,
-    //         ]));
-    //     });
-
-    //     $writer->close();
-
-    //     return response()->download($filePath);
-
-    // }
+    }
 }
