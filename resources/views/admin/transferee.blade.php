2eeba13428c75f363283688f6960d0f294280b04
deejayanareta33 <deejayanareta33@gmail.com>
2025-05-07 12:33:12 +0800

    ADD views for student categories: Dropped Out, Failed, Graduated, Octoberian, Returnee, and Transferee with respective tables and data display

---
+++ resources/views/admin/transferee.blade.php
@@ -0,0 +1,43 @@
+<x-app-layout>
+
+    <x-page-title header="Transferee Students" :links="['transferee' => '/transferee']"/>
+
+    <div class="card p-3">
+        <table id="transfereetable" class="table table-striped datatable">
+            <thead>
+                <tr>
+
+                    <th>No.</th>
+                    <th>Name</th>
+                    <th>Student No</th>
+                    <th>Age</th>
+                    <th>Email</th>
+                    <th>Action</th>
+                </tr>
+            </thead>    
+            <tbody>
+                @foreach ( $students as $index => $student)
+                @if ($student->enrollment_status === 'Transferee')
+                <tr>
+                    <td>{{$index + 1}}</td>
+                    <td class="text-capitalize">
+                        {{ $student->last_name ?? 'N/A'}},
+                        {{ $student->first_name ?? 'N/A'}}
+                        {{ $student->middle_name ?? 'N/A'}}
+                    </td>
+                    <td>{{ $student->student_number ?? 'N/A'}}</td>
+                    <td>{{ $student->age ?? 'N/A' }}</td>
+                    <td>{{ $student->email_address ?? 'N/A' }}</td>
+                    <td>
+                        <a href="/student/profile/{{$student->id}}" class="btn btn-primary">
+                            <i class="fa-solid fa-eye"></i>
+                        </a>
+                    </td>
+                </tr>
+                @endif
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
+</x-app-layout>
